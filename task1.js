41e22972c62ce05b40c6f7328139c9cd24ecd143
AfricanJay <onyeamajennifer@gmail.com>
2020-03-24 21:49:11 +0000

    JavaScript

---
+++ task1.js
@@ -0,0 +1,16 @@
+
+
+let name = 'AfricanJay';
+
+const courses = ['HTML', 'CSS', 'JavaScript', 'PHP'];
+
+console.log(name, courses);
+
+//To print all even numbers between 1 and 200 (FROM 2 TO 200)
+
+for(let i = 0; i <= 198; i+=0){
+  i = i + 2;
+console.log(i);
+}
+
+
